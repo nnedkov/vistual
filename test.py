e19e3f43c6a628f48f41096f7e652052f84429ff
Nedko Nedkov <nedko.stefanov.nedkov@gmail.com>
2020-09-27 12:56:33 +0200

    Added scripted and static dashboards for proof of concept

--- test.py
+++ test.py
@@ -9,12 +9,12 @@ __author__ = 'nnedkov'
 
 
 if __name__ == '__main__':
-    v_logger = VistualLogger('vistual-logger-test')
+    v_logger = VistualLogger('vistual-logger-test', recreate_db=False)
     v_logger.annotate('And there was light...')
 
     for _ in range(10):
         value = randint(0, 10)
-        v_logger.log(value)
+        v_logger.log(value, tag='test0')
         sleep(1)
 
     v_logger.annotate('Shutdown...')
